3babbcb5cf2380fbf2a5fc6b77fb54ce0a1617d2
jslsn <jordan.s.layne@gmail.com>
2021-08-19 10:46:36 +0100

    Commented and slightly restructured Rock Paper Scissors

--- git/rock_paper_scissors/Rock_Paper_Scissors.py
+++ git/rock_paper_scissors/Rock_Paper_Scissors.py
@@ -1,123 +1,129 @@
+#Import the random functions
 import random
 
-RPS=('Rock','Paper','Scissors')
-
+#Prompt the user 
 print('Hey! Wanna play rock paper scissors!?')
 
-yes_list=('Yes','yes','YES','yEs','yeS','YEs','yES','Sure','sure','yeah','Yeah','Ready','ready')
-
-no_list=('No','no','NO','nO','Nah','nah')
-
-answer_check=False
-
+#A variable to control how the function bellow will work in different scenarios and another to control the loop.
+prompt_switch=0
 user_perm=True
 
+#A function that will check the users input and respond appropriately.
 def yes_no():
-    global yes_list
-    global no_list
+    #import these two variables into the function.
+    global prompt_switch
+    global user_perm
+    #Lists that allow for more variation in possible answers a user can enter.
+    yes_list=('Yes','yes','YES','yEs','yeS','YEs','yES','Sure','sure','yeah','Yeah','Ready','ready','y','Y')
+    no_list=('No','no','NO','nO','Nah','nah','n','N')
     check=False
+    #While we haven't been given the a (valid) answer, ask the user to answer "Yes or no?"
     while check!=True:
         print('Yes or no?')
         answer=input()
+        #If the user answers yes, set the check to be true and break the loop.
         if answer in yes_list:
             check=True
+        #If the answer is no and we're not in the loop, close the script.
         elif answer in no_list:
-            print ('Oh, okay, cya!')
-            exit()
+            if prompt_switch==0:
+                print ('Oh, okay, cya!')
+                exit()
+            #And if we are in the loop, break this one and the larger one it's in.
+            else:
+                print("Okay, this was fun, so come back when you're ready for more")
+                check=True
+                user_perm=False
+        #If the answer we get is invalid, restart and ask them to say either "Yes or no?"
         else:
             check=False
 
+#Use this function to prompt the user as they're introducing the game to them.
 yes_no()
 print ("Okay, let's go!")
-answer_list=('Rock','Paper','Scissors')
 print('When I say scissors, type in your answer!')
 print('Ready?')
 yes_no()
 
+#Set up the scores so that it can be kept track of
 user_score=0
 computer_score=0
 
+#While the computer still has the users permission to continue.
 while user_perm==True:
-    """
-    while answer_check!=True:
-        print('Yes or no?')
-        user_answer=input()
-        if user_answer in yes_list:
-            answer_check=True
-            user_perm=True
-        elif user_answer in no_list:
-            print ('Oh, okay, cya!')
-            exit()
-        else:
-            answer_check=False
-    
-    yes_no()
-    print ("Okay, let's go!")
-    answer_list=('Rock','Paper','Scissors')
-    print('When I say scissors, type in your answer!')
-    print('Ready?')
-    yes_no()
-    """
-
-
+    #Switch the functionality of the function so it works in loops and begin the game
+    prompt_switch=1
     print('Okay!')
     print ('Rock')
     print('Paper')
     print('Scissors!')
+    #Users answer is taken in
     user_choice=input()
+    #Pick a random answer and make it the computer selection
+    answer_list=('Rock','Paper','Scissors')
     computer_choice= random.choice(answer_list)
+    #New function will assign a value depending on the answer which will allow for the game to be played.
     def assign_val(choice):
-        rock_list=('Rock','rock')
-        paper_list=('Paper','paper')
-        scissor_list=('Scissors','scissors')
+        #Variations of ways to type each valid response
+        rock_list=('Rock','rock','r')
+        paper_list=('Paper','paper','p')
+        scissor_list=('Scissors','scissors','s')
+        #If rock, the value returned will be 2
         if choice in rock_list:
             return 2
+        #If paper, the value returned will be 6
         elif choice in paper_list:
             return 6
+        #If scissors, the value returned will be -4
         elif choice in scissor_list:
             return -4
+        #If none of these have been entered, a string, "Invalid" Will be returned
         else:
             return "Invalid"
-    
+
+    #Give both the user and computer's answers the correct value(using the assign_val function)
     user_choice_value= assign_val(user_choice)
     computer_choice_value= assign_val(computer_choice)
 
+    #If the function returns as "Invalid" on the user end, prompt them repeatedly until they answer correctly
     while user_choice_value=="Invalid":
         print("It seems you entered something that isn't valid in this game, try entering rock, paper or scissors")
         user_choice=input()
         user_choice_value= assign_val(user_choice)
 
+    #We determine who won by creating two results, created by dividing the choices by each other in both ways
     result1=computer_choice_value/user_choice_value
     result2=user_choice_value/computer_choice_value
 
+    #Now we print what the computer chose so that the user can see.
     print (computer_choice)
-        
+
+    #If both values are the same then it's obviously a draw
     if user_choice_value==computer_choice_value:
         print("Tied, dang")
-        
+    #If the computers value divided by the user value is larger than the opposite, the computer has won.
     elif result1 > result2:
         print ('I Win!')
+        #Increase the computers score
         computer_score+= 1
+    #Likewise, if the users value divided by the computers value is larger than the opposite, they've won this round
     elif result2 > result1:
         print('You win!')
+        #Increase the users score
         user_score+= 1
-        
+
+    #Print out the scores in ways that vary depending on score
     if user_score > computer_score:
         print (f"You're currently winning {user_score}-{computer_score} :(")
     elif computer_score > user_score:
         print (f"I'm currently winning {computer_score}-{user_score} :D")
     else:
         print (f"We're currently tied up at {computer_score}-{computer_score}, we should keep going and find a winner")
-        
+
+    #Prompt the user to do another round(using the switched up yes_no function)     
     print('Wanna go again!?')
 
     yes_no()
 
 
     
-
-
-
-
-#Try to play it in a loop so that the user can easily go again. Maybe keep score too?
-
